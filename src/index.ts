25e9359858c6da3ca5b0e2079fc229e08e1a537c
Alberto Siurob <asiurob@gmail.com>
2024-11-08 20:48:32 -0600

    Added user model and basis for user route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Server } from './core/server'
 import { Database } from './core/database'
+import UsersRoute from './routes/users.route'
 
 const server   = Server.instance
 const database = Database.instance
 
 server.start()
 database.connect()
+
+
+server.app.use( '/users', UsersRoute )
